f7487d288115c384e0b9433862b0692cd88307b6
Deepak <deepakcr037@gmail.com>
2026-08-07 14:25:11 +0530

    Finalize furniture store project for deployment

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('address/add/', views.add_address_view, name='add_address'),
     path('address/delete/<int:pk>/', views.delete_address_view, name='delete_address'),
     path('address/default/<int:pk>/', views.set_default_address_view, name='set_default_address'),
+    
 ]
